f2c42274dc8db6c8e3c0a2d5c9bc24f22b220277
bowle <bowle@10.220.41.73>
2024-10-10 22:12:24 -0400

    i love coding!!

--- src/edu/wit/cs/comp1000/PA2a.java
+++ src/edu/wit/cs/comp1000/PA2a.java
@@ -1,11 +1,35 @@
 package edu.wit.cs.comp1000;
 
+import java.util.Scanner;
+
 // TODO: document this class
 public class PA2a {
 
 	// TODO: document this function
 	public static void main(String[] args) {
-		// TODO: write your code here
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Enter five whole numbers:");
+		
+		double num1 = scanner.nextDouble();
+		double num2 = scanner.nextDouble();
+		double num3 = scanner.nextDouble();
+		double num4 = scanner.nextDouble();
+		double num5 = scanner.nextDouble();
+		
+		double sum = 0;
+		double posSum = 0;
+		double negSum = 0;
+		double avg = 0;
+		double posAvg = 0;
+		double negAvg = 0;
+		
+		sum = num1 + num2 + num3 + num4 +num5;
+		
+		if (num1 > 0)
+			sum += num1;
+		else negSum += num1;
 	}
 
 }
